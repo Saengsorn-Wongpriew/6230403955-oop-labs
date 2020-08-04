dd534cfc5514064bcb08f5ab6623daaaaaeb24c7
Saengsorn-Wongpriew <69130885+Saengsorn-Wongpriew@users.noreply.github.com>
2020-08-04 10:50:00 +0700

    Update find_avg_std.py

--- saengsorn-6230403955-lab1/find_avg_std.py
+++ saengsorn-6230403955-lab1/find_avg_std.py
@@ -2,13 +2,8 @@ import random
 
 a = random.randint(1, 10)
 b = random.randint(1, 10)
-
-a, b = int(a), int(b)
-
 avg = (a + b)/2
-
 std = (((a-avg)**2 + (b-avg)**2)/2)**0.5
 
 print("The average of", a, "and", b, "is", avg)
-
 print("The standard deviation of", a, "and", b, "is", std)
