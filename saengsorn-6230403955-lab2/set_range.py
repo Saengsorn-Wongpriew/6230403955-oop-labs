88114d317cee0f068b1fee4ee9b68fd3013579df
Saengsorn-Wongpriew <69130885+Saengsorn-Wongpriew@users.noreply.github.com>
2020-08-11 10:01:56 +0700

    Update set_range.py

--- saengsorn-6230403955-lab2/set_range.py
+++ saengsorn-6230403955-lab2/set_range.py
@@ -16,22 +16,16 @@ print("a | b is", c)
 print("a - b is", d)
 
 a = []
-
 for n in range(20):
     a.append(n)
-
 print(a)
 
 b = []
-
 for n in range(3, 13):
     b.append(n)
-
 print(b)
 
 c = []
-
 for n in range(2, 53, 3):
     c.append(n)
-
 print(c)
