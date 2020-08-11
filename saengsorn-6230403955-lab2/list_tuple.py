33062ba5c2114b58b1a7a839371cb51bfd451ce6
Saengsorn-Wongpriew <69130885+Saengsorn-Wongpriew@users.noreply.github.com>
2020-08-11 09:57:05 +0700

    Update list_tuple.py

--- saengsorn-6230403955-lab2/list_tuple.py
+++ saengsorn-6230403955-lab2/list_tuple.py
@@ -7,8 +7,8 @@ v = []
 
 for i in range(5):
     for k in range(10):
-        v.append(k+((i-1)*10))
+        v.append(k + ((i - 1) * 10))
     v = []
     if i < 4:
         w.append(v)
-print([w[0][-2] , w[0][-1]])
+print([w[0][-2], w[0][-1]])
