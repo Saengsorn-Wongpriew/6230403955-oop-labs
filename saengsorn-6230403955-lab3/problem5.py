e4bab4eca51c20391fb7147545ccb8d690d5f899
Saengsorn-Wongpriew <69130885+Saengsorn-Wongpriew@users.noreply.github.com>
2020-08-18 10:47:40 +0700

    Update problem5.py

--- saengsorn-6230403955-lab3/problem5.py
+++ saengsorn-6230403955-lab3/problem5.py
@@ -1,4 +1,5 @@
-fruits = (list(enumerate(["Grapefruit", "Longan", "Orange", "Apple", "Cherry"])))
+fruits = ["Grapefruit", "Longan", "Orange", "Apple", "Cherry"]
+emu_fruits = list(enumerate(fruits, start=1))
 
-for i in fruits:
-    print(i)
+for i in range(len(emu_fruits)):
+    print(str(emu_fruits[i][0]) + ". " + str(emu_fruits[i][1]))
