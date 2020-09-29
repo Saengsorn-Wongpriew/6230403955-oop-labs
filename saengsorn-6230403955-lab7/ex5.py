2330aa3829cae7ac86bbdf78817609ea6d434171
Saengsorn-Wongpriew <69130885+Saengsorn-Wongpriew@users.noreply.github.com>
2020-09-29 11:16:36 +0700

    Update ex5.py

--- saengsorn-6230403955-lab7/ex5.py
+++ saengsorn-6230403955-lab7/ex5.py
@@ -1,36 +1,38 @@
-import math
+class Numbers:
 
+    def __init__(self, n1, n2):
+        self.x = n1
+        self.y = n2
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+    def add(self):
+        return "{} + {} = {}".format(self.x, self.y, (self.x + self.y))
 
-    def area(self):
-        return math.pi * math.pow(self.radius, 2)
+    @classmethod
+    def display_factors(cls, number):
+        if number % 2 == 0:
+            cls.display = number
+            return "Factors of {} is {} and {}".format(
+                cls.display, cls.display/2, cls.display - cls.display/2
+            )
+        else:
+            cls.display = number
+            return "Factors of {} is {} and {}".format(
+                cls.display,
+                (cls.display - 1) / 2,
+                cls.display - (cls.display + 1) / 2
+            )
 
-    def perimeter(self):
-        return 2 * math.pi * self.radius
+    @staticmethod
+    def is_valid_divisor(number):
+        if number == 0:
+            return "{} is not a valid devisor".format(number)
+        else:
+            return "{} is a valid devisor".format(number)
 
 
 if __name__ == "__main__":
-    c_circle = Circle(3)
-    print(
-        "The circle with radius {} "
-        "has the area as {:.2f} "
-        "and the perimeter as {:.2f}".format(
-            c_circle.radius,
-            c_circle.area(),
-            c_circle.perimeter()
-            )
-    )
-
-    c_circle = Circle(4)
-    print(
-        "The circle with radius {} "
-        "has the area as {:.2f} "
-        "and the perimeter as {:.2f}".format(
-            c_circle.radius,
-            c_circle.area(),
-            c_circle.perimeter()
-            )
-    )
+    print(Numbers(3, 5).add())
+    print(Numbers.display_factors(6))
+    print(Numbers.display_factors(8))
+    print(Numbers.is_valid_divisor(2))
+    print(Numbers.is_valid_divisor(0))
