9f56c836a357c67617bd110c9ab0cb7021bce4e1
Saengsorn-Wongpriew <69130885+Saengsorn-Wongpriew@users.noreply.github.com>
2020-11-09 10:42:48 +0700

    learning gui

---
+++ sample/ex3.py
@@ -0,0 +1,55 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit
+from PyQt5.QtWidgets import QVBoxLayout, QRadioButton, QPushButton
+from PyQt5.QtWidgets import QFormLayout, QHBoxLayout
+
+
+class FormLayout(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        label_name = QLabel("Name")
+        edit_name = QLineEdit()
+
+        fbox = QFormLayout()
+        fbox.addRow(label_name, edit_name)
+        vbox = QVBoxLayout()
+
+        label_addr = QLabel("Address")
+        addr1 = QLineEdit()
+        addr2 = QLineEdit()
+
+        vbox.addWidget(addr1)
+        vbox.addWidget(addr2)
+        fbox.addRow(label_addr, vbox)
+
+        hbox = QHBoxLayout()
+        radio_male = QRadioButton("Male")
+        radio_female = QRadioButton("Female")
+        hbox.addWidget(radio_male)
+        hbox.addWidget(radio_female)
+        hbox.addStretch()
+        fbox.addRow(QLabel("Gender"), hbox)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(QPushButton("Submit"))
+        hbox.addWidget(QPushButton("Cancel"))
+        fbox.addRow(hbox)
+
+        self.setLayout(fbox)
+        self.setWindowTitle("FormLayout")
+        self.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = FormLayout()
+    print(ex.isActiveWindow)
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
---
+++ sample/prob1.py
@@ -0,0 +1,53 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtWidgets import QFormLayout, QVBoxLayout, QLineEdit
+from PyQt5.QtWidgets import QRadioButton, QHBoxLayout, QPushButton
+
+
+class SimpleForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("FormLayout")
+        self.init_line_edit()
+
+    def init_line_edit(self):
+        fbox = QFormLayout()
+        self.setLayout(fbox)
+        label_sf = QLabel('SimpleForm')
+        label_sf.setStyleSheet("color: blue; font: bold 20px")
+
+        vbox = QVBoxLayout()
+        fbox.addRow(None, label_sf)
+
+        label_name = QLabel('Name')
+        fbox.addRow(label_name, vbox)
+
+        edit_name = QLineEdit()
+        edit_name.setMinimumWidth(300)
+        fbox.addRow(label_name, edit_name)
+        
+        hbox = QHBoxLayout()
+        radio_male = QRadioButton("Male")
+        radio_female = QRadioButton("Female")
+        hbox.addWidget(radio_male)
+        hbox.addWidget(radio_female)
+        hbox.addStretch()
+        fbox.addRow(QLabel("Gender"), hbox)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(QPushButton("Submit"))
+        hbox.addWidget(QPushButton("Cancel"))
+        fbox.addRow(hbox)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = SimpleForm()
+    ex.show()
+    print(ex.isActiveWindow())
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
